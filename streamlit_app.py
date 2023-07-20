03318e3f3371c7a1fee98e51d8fa230c1fce11e4
ant.sakalouski <ant.sakalouski@gmail.com>
2023-07-20 14:44:32 +1000

    adding and showing dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit
+import pandas
 
 streamlit.title("Hello, snowflake")
 streamlit.header("My header")
 streamlit.text("Sample text")
+
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
